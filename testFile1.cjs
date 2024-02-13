214391901a875c551fdeef42a91a8f0bcde6c24b
Vaidehis3007 <126815536+Vaidehis3007@users.noreply.github.com>
2024-02-13 15:05:48 +0000

    Commit for requirement 1 was made

--- testFile1.cjs
+++ testFile1.cjs
@@ -1,9 +1,5 @@
 // testFile1.js
 
 const { isLeapYear } = require('./dateUtil.cjs');
-
-// Testing a leap year
 console.log(isLeapYear(2020)); // true
-
-// Testing a non-leap year
 console.log(isLeapYear(2021)); // false
--- testFile2.cjs
+++ testFile2.cjs
@@ -1,9 +1,5 @@
 // testFile2.js
 
 const dateUtil = require('./dateUtil.cjs');
-
-// Testing a leap year
 console.log(dateUtil.isLeapYear(2020)); // true
-
-// Testing a non-leap year
 console.log(dateUtil.isLeapYear(2021)); // false
