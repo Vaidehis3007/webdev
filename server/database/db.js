ae048b3a53c27f0ea5626d9314ace65c0f104d3c
Vaidehis3007 <126815536+Vaidehis3007@users.noreply.github.com>
2024-05-09 00:37:10 +0530

    Database was created

--- server/database/db.js
+++ server/database/db.js
@@ -1,8 +1,6 @@
 // MongoDB password - c17brCdYBEdcW6Gy
 import mongoose from "mongoose"
 
-
-
 export const Connection = async () => {
     const password = "c17brCdYBEdcW6Gy";
     const URL = `mongodb+srv://sahuvs:${password}@blog-app.h0zmlfw.mongodb.net/?retryWrites=true&w=majority&appName=blog-app`;
