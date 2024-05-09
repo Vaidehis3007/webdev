466e053581c9dc4546b62fdfa60b4f94d2bede47
Vaidehis3007 <126815536+Vaidehis3007@users.noreply.github.com>
2024-05-09 12:49:22 +0530

    API notification messaged were created

---
+++ client/src/constants/config.js
@@ -0,0 +1,23 @@
+//API_NOTIFICATON_MESSAGES
+export const API_NOTIFICATON_MESSAGES = {
+    loading: {
+        title: 'Loading...',
+        message: 'Data is being loaded, Please wait'
+    },
+    success: {
+        title: 'Success',
+        message: 'Data successfully loaded'
+    },
+    responseFailure: {
+        title: 'Error',
+        message: 'An error occured while fetching response from the server. Please try again'
+    },
+    requestFailure: {
+        title: 'Error',
+        message: 'An error occured while parsing the requested data'
+    },
+    networkError: {
+        title:'Error',
+        message:'Unable to connect with the server. PLease check the internet connection and try again.'
+    }
+}
