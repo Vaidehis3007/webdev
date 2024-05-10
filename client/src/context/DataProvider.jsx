c5badb0ea1a69be307f4de3e45a4a4f5ceaebe7f
Vaidehis3007 <126815536+Vaidehis3007@users.noreply.github.com>
2024-05-10 16:09:06 +0530

    Created DataProvider

---
+++ client/src/context/DataProvider.jsx
@@ -0,0 +1,20 @@
+import { createContext, useState } from "react"
+
+export const DataContext = createContext(null);
+
+const DataProvider = () => {
+
+    cosnt [getAccordionUtilityClass, setAccount] = useState({username: '', name: ''})
+
+    return (
+        <DataContext.Provider value={{
+            account,
+            setAccount
+        }}>
+
+        </DataContext.Provider>
+    )
+
+}
+
+export default DataProvider;
